033b0d78ff9ae5b9e0436825304b617ded3cd2a1
ROHIT4321 <rppatil3210@gmail.com>
2017-05-09 19:26:22 +0530

    Test

--- GitHub_Integration/src/GIT_HUB.java
+++ GitHub_Integration/src/GIT_HUB.java
@@ -3,7 +3,7 @@ public class GIT_HUB {
 
 	public static void main(String[] args) {
 	
-		for (int j = 0; j < args.length; j++) {
+		for (int j = 0; j < 10 ; j++) {
 			
 			System.out.println(j);
 			
